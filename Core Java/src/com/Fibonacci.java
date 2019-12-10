adcf7664923234ef6527a69a0a061592d96b520f
Nikhil Bhardwaj <Nikhil Bhardwaj@NikhilBhardwaj>
2019-12-10 22:25:11 +0530

    fibnocci series program update

--- Core Java/src/com/Fibonacci.java
+++ Core Java/src/com/Fibonacci.java
@@ -1,10 +1,28 @@
 package com;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int i, f1, f2 = 0, f3 = 1, limit;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("How Many Numbers Do you Want to Print..?");
+		limit = sc.nextInt();
+		if (limit == 0 || limit == 1) {
+			System.out.print(f2 + " " + f3);
+		} else {
+			System.out.print(f2 + " " + f3);
+
+			for (i = 0; i < limit; i++) {
+				f1 = f2;
+				f2 = f3;
+				f3 = f1 + f2;
+				System.out.print(" " + f3);
+			}
+		}
 	}
 
 }
